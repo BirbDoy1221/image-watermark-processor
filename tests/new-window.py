089aa3684936db5a4ccf831f94cc0b98c218b415
Chanz Jryko <langreo.grim@gmail.com>
2025-02-27 07:26:21 +0800

    yes

--- tests/new-window.py
+++ tests/new-window.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+
+
 root = Tk()
 root.title('New Window')
 
@@ -8,7 +10,12 @@ mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
 root.columnconfigure(index=0, weight=1)
 root.rowconfigure(index=0, weight=1)
 
-new_window = Button(mainframe, text='New Window', command=root.destroy)
+def newWindow():
+    new_frame = Toplevel(root)
+
+new_window = Button(mainframe, text='New Window', command=newWindow)
 new_window.grid(column=1, row=1)
+
+
 
 root.mainloop()
